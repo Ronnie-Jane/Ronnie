452970a259f6d9097173e7b6bc48121781adc8ac
Ronnie-jane <374698465@qq.com>
2019-12-01 16:03:01 +0800

    add fac function

--- new.py
+++ new.py
@@ -21,3 +21,10 @@ def divide(a, b):
         return a/b
     except:
         raise TypeError
+
+def factorial(a):
+    assert type(a) is int
+    if a == 1:
+        return a
+    else:
+        return a*factorial(a-1)
